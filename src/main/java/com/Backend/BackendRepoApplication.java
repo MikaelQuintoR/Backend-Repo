c6574ffa69ea606bdaaaade808f1ae672a321bd0
MikaelQuintoR <MIKAEL.QUINTO@UTEC.EDU.PE>
2024-10-02 17:30:34 -0500

    HotFix 1.0

--- src/main/java/com/Backend/BackendRepoApplication.java
+++ src/main/java/com/Backend/BackendRepoApplication.java
@@ -1,4 +1,4 @@
-package com.backendrepo;
+package com.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/Backend/BackendRepoApplicationTests.java
+++ src/test/java/com/Backend/BackendRepoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.backendrepo;
+package com.Backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
